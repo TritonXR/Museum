330c832a731a544eec93b42d49cc0362bc2bbcea
Wanze Xie <waxie@UCSD.EDU>
2016-10-12 15:47:23 -0700

    Add Activate.cs script

---
+++ Assets/MyScripts/Activate.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using VRTK;
+using System.Collections;
+
+public class Activate : MonoBehaviour {
+
+
+    private GameObject objActivate;
+
+    void Start() {
+        objActivate = GetComponentInChildren<VRTK_ObjectTooltip>().gameObject;
+    }
+
+	// Update is called once per frame
+	void Update () {
+
+
+        if (Input.GetKeyDown(KeyCode.A)) {
+        
+            objActivate.SetActive(!objActivate.activeSelf);
+            
+        }
+
+    }
+}
