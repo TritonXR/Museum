ee61562158d941b377f40640ff7d86e29f967978
Marlo D Boticano <mbotican@UCSD.EDU>
2016-10-26 15:16:07 -0700

    Made new scene with fokkerd7 and rotating propeller script

---
+++ Assets/rotate.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class rotate : MonoBehaviour {
+    // numDegrees is the rotation per second
+    public int numDegrees = 1;
+    // spinFaster is how many degrees it'll rotate faster
+    public int spinFaster = 2;
+    // speedMax is what's the max degrees it should rotate per second
+    public int speedMax = 3600;
+    // whatAxis is the vector/axis we're going to rotate
+    public Vector3 whatAxis = Vector3.forward;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(whatAxis * numDegrees * Time.deltaTime);
+
+        while (numDegrees < speedMax){
+            numDegrees += spinFaster;
+            break;
+        }
+    }
+}
