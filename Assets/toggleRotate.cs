a3e9195d624ca1c99f383871351bdfb19ea11bcc
Marlo D Boticano <mbotican@UCSD.EDU>
2016-10-28 15:08:27 -0700

    added script for rudder, elevator, and aileron flapping motion

---
+++ Assets/toggleRotate.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class toggleRotate : MonoBehaviour
+{
+    // numDegrees is the rotation per second
+    public int numDegrees = 1;
+    // spinFaster is how many degrees it'll rotate faster
+    //public int spinFaster = 2;
+    // speedMax is what's the max degrees it should rotate per second
+    //public int speedMax = 3600;
+    // whatAxis is the vector/axis we're going to rotate
+    public Vector3 whatAxis = Vector3.forward;
+
+    public int maxRotate = 15;
+    public int minRotate = 0;
+    public int curRotate = 0;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(whatAxis * numDegrees * Time.deltaTime);
+
+        curRotate += numDegrees; 
+
+        if (curRotate == maxRotate || curRotate == minRotate)
+        {
+            numDegrees = numDegrees * -1; //toggle direction
+        }
+    }
+}
