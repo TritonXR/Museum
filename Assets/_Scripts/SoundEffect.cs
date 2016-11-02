bb195ba2b4cc419c03d3d40ad9f53903826aa6ea
Wanze Xie <waxie@UCSD.EDU>
2016-11-02 13:01:40 -0700

    Basically sound API working

---
+++ Assets/_Scripts/SoundEffect.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundEffect : MonoBehaviour
+{
+
+    public AudioClip TakeOffSound;
+    public AudioClip EngineSound;
+    public AudioClip StartSound;
+
+
+    private AudioSource m_TakeOff;
+    private AudioSource m_Engine;
+    private AudioSource m_Start;
+
+    void Start()
+    {
+        m_TakeOff = this.gameObject.AddComponent<AudioSource>();
+        m_TakeOff.clip = TakeOffSound;
+
+        m_Engine = this.gameObject.AddComponent<AudioSource>();
+        m_Engine.clip = EngineSound;
+
+        m_Start = this.gameObject.AddComponent<AudioSource>();
+        m_Start.clip = StartSound;
+
+    }
+
+    public void PlayTakeOff()
+    {
+        
+        m_TakeOff.Play();
+    }
+
+    public void PlayEngine()
+    {
+        m_Engine.loop = true;
+        m_Engine.Play();
+    }
+}
