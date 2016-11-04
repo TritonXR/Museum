e073543eccd51180899a5200dedaf7941f44b8a2
Wanze Xie <waxie@UCSD.EDU>
2016-11-04 16:14:16 -0700

    test steamvr

---
+++ Assets/_Scripts/ActivateByController.cs
@@ -0,0 +1,33 @@
+﻿namespace VRTK.Examples
+{
+    using UnityEngine;
+    using System.Collections;
+
+    public class ActivateByController : VRTK_InteractableObject
+    {
+        public AudioClip myClip;
+        private GameObject objActivate;
+        
+        protected override void Start()
+        {
+            objActivate = GetComponentInChildren<VRTK_ObjectTooltip>().gameObject;
+            objActivate.SetActive(false);
+
+            this.gameObject.AddComponent<AudioSource>();
+            this.GetComponent<AudioSource>().clip = myClip;
+
+        }
+
+        public override void StartUsing(GameObject currentUsingObject)
+        {
+            base.StartUsing(currentUsingObject);
+            objActivate.SetActive(true);
+        }
+
+        public override void StopUsing(GameObject previousUsingObject)
+        {
+            base.StopUsing(previousUsingObject);
+            objActivate.SetActive(false);
+        }
+    }
+}
