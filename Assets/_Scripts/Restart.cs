0772084e737b77909e9e4237757c24cfccc22032
Wanze Xie <waxie@UCSD.EDU>
2016-11-14 16:52:35 -0800

    Try to implementing restart

---
+++ Assets/_Scripts/Restart.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Restart : MonoBehaviour {
+    
+    void OnGUI()
+    {
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            Debug.Log("LALALAL");
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+}
