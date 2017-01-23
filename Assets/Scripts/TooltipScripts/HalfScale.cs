ddfef3c9896723ad250e482c3ae62e52cd8e716a
Wanze Xie <waxie@UCSD.EDU>
2017-01-23 15:17:57 -0800

    did scene for halfscale test

---
+++ Assets/Scripts/TooltipScripts/HalfScale.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HalfScale : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            if(transform.localScale.x < 0.002)
+            {
+                
+            }
+            if (transform.localScale.x < 0.02)
+            {
+                transform.localScale -= new Vector3(0.001f, 0.001f, 0.001f);
+            }
+            else
+            {
+                transform.localScale -= new Vector3(0.01f, 0.01f, 0.01f);
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+
+            if (transform.localScale.x < 0.02)
+            {
+                transform.localScale += new Vector3(0.001f, 0.001f, 0.001f);
+            }
+            else
+            {
+                transform.localScale += new Vector3(0.01f, 0.01f, 0.01f);
+            }
+        }
+
+    }
+}
