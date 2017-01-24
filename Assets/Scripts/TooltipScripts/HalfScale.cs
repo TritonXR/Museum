709933d24255e85e9d01af4043ec6a3c1a994e92
Wanze Xie <waxie@UCSD.EDU>
2017-01-23 17:17:37 -0800

    Did ScrollBar

--- Assets/Scripts/TooltipScripts/HalfScale.cs
+++ Assets/Scripts/TooltipScripts/HalfScale.cs
@@ -1,17 +1,63 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HalfScale : MonoBehaviour {
 
+    public Scrollbar bar;
+    private float currScale;
+
 	// Use this for initialization
 	void Start () {
-		
+        bar.value = (transform.localScale.x - 0.01f)/(0.19f); 
 	}
 	
+    IEnumerator ScalingPlane()
+    {
+        while (true)
+        {
+            currScale = bar.value * 0.19f + 0.01f;
+            transform.localScale = new Vector3(currScale, currScale, currScale);
+            yield return null;
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetKeyDown(KeyCode.Q))
+
+// uncomment to scale by keyboard
+/*        if (Input.GetKeyDown(KeyCode.Q))
         {
             if(transform.localScale.x < 0.002)
             {
@@ -38,6 +84,8 @@ public class HalfScale : MonoBehaviour {
                 transform.localScale += new Vector3(0.01f, 0.01f, 0.01f);
             }
         }
+
+*/
 
     }
 }
