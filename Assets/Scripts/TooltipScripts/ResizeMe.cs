76e1f223139ff3ef59bc86c2177fa3164fcd3ff4
Wanze Xie <waxie@UCSD.EDU>
2017-01-30 16:19:23 -0800

    lalala

--- Assets/Scripts/TooltipScripts/ResizeMe.cs
+++ Assets/Scripts/TooltipScripts/ResizeMe.cs
@@ -8,10 +8,9 @@ public class ResizeMe : MonoBehaviour {
     //public SteamVR_Camera head;
     public float maximumDiff;
 
-    private float currentDiff;
-    private float currentSize;
+    private float currSize;
     private float currentHeight;
-    private float originalSize;
+    private float prevSize;
     private Vector3 currentV;
     //private SteamVR_TrackedObject trackedObj;
     private SteamVR_Controller.Device rightDevice;
@@ -20,42 +19,64 @@ public class ResizeMe : MonoBehaviour {
 	void Start () {
         int rightIndex = SteamVR_Controller.GetDeviceIndex(SteamVR_Controller.DeviceRelation.Rightmost);
         rightDevice = SteamVR_Controller.Input(rightIndex);
-        originalSize = player.transform.localScale.x;
-        currentV = player.transform.localScale;
 
-        StartCoroutine(CheckingResize());
+        currentV = player.transform.localScale;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
 
-    IEnumerator CheckingResize()
-    {
-        Debug.Log("Get in to the Resize function");
-        while (true)
-        {
-            currentHeight = player.transform.position.y;
-            while (rightDevice.GetTouch(SteamVR_Controller.ButtonMask.Touchpad)){
-                //Debug.Log("Touch Pad is Pressed");
-                currentDiff = rightDevice.GetAxis().y - currentHeight ;
-                //Debug.LogFormat("Yaxis of right controller is: {0}", rightDevice.GetAxis().y);
-                currentSize = originalSize * currentDiff;
-
-
-                //if (currentSize > 0)
-                //{
-                currentV.x = currentSize;
-                currentV.y = currentSize;
-                currentV.z = currentSize;
-                player.transform.localScale = currentV;
-                //}
-
-                yield return null;
-            }
-            yield return null;
+        if (rightDevice.GetPressDown(SteamVR_Controller.ButtonMask.Touchpad)){
+            prevSize = player.transform.localScale.x;
+            currentHeight = rightDevice.GetAxis().y;
         }
 
+        if (rightDevice.GetPress(SteamVR_Controller.ButtonMask.Touchpad))
+        {
+            Resize();
+        }
+
+        if (rightDevice.GetPressUp(SteamVR_Controller.ButtonMask.Touchpad))
+        {
+            prevSize = player.transform.localScale.x;
+
+
+        }
+
+    }
+
+    //IEnumerator CheckingResize()
+    //{
+    //    Debug.Log("Get in to the Resize function");
+    //    while (true)
+    //    {
+    //        currentHeight = player.transform.position.y;
+    //        while (rightDevice.GetTouch(SteamVR_Controller.ButtonMask.Touchpad)){
+    //            //Debug.Log("Touch Pad is Pressed");
+    //            currentDiff = rightDevice.GetAxis().y - currentHeight ;
+    //            //Debug.LogFormat("Yaxis of right controller is: {0}", rightDevice.GetAxis().y);
+    //            currentSize = originalSize * currentDiff;
+
+
+    //            //if (currentSize > 0)
+    //            //{
+    //            currentV.x = currentSize;
+    //            currentV.y = currentSize;
+    //            currentV.z = currentSize;
+    //            player.transform.localScale = currentV;
+    //            //}
+
+    //            yield return null;
+    //        }
+    //        yield return null;
+    //    }
+
+    //}
+
+    void Resize()
+    {
+        float updatedHeight = rightDevice.GetAxis().y;
+        currSize = 5 * ((updatedHeight - currentHeight) / currentHeight) * prevSize;
+        player.transform.localScale = new Vector3(currSize, currSize, currSize);
     }
 }
