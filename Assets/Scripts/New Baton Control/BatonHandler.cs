6b40012c1141b493d374a9d2770d2c62d1d42e80
Wanze Xie <waxie@UCSD.EDU>
2017-02-03 18:06:43 -0800

    most baton movements working

--- Assets/Scripts/New Baton Control/BatonHandler.cs
+++ Assets/Scripts/New Baton Control/BatonHandler.cs
@@ -5,17 +5,21 @@ using UnityEngine;
 public class BatonHandler : MonoBehaviour {
     
     //Combination Panels 
-    public BatonControl LPanel;
-    public BatonControl RPanel;
-
+    public BatonControl ULPanel;
+    public BatonControl URPanel;
+    public BatonControl BLPanel;
+    public BatonControl BRPanel;
 
     public static BatonHandler instance;
     public PlaneGrabber planeGrabber;
     public GameObject plane;
 
 
-    public float movementMult = 5.0f;
-    public float dur = 1.0f;
+    public float movementMult = 3.0f;
+    public float dur = 0.5f;
+
+    bool moving;
+    bool rotating;
 	// Use this for initialization
 	void Start () {
         if (instance == null)
@@ -26,6 +30,7 @@ public class BatonHandler : MonoBehaviour {
         {
             Destroy(this);
         }
+        moving = false;
 	}
 	
 	// Update is called once per frame
@@ -35,19 +40,72 @@ public class BatonHandler : MonoBehaviour {
 
     public void checkCombinations()
     {
-        if(LPanel.active && RPanel.active)
+        //Forward
+        plane = planeGrabber.getPlaneAtGaze();
+
+        if (ULPanel.active && URPanel.active)
         {
-            plane = planeGrabber.getPlaneAtGaze();
+            if (moving)
+            {
+                moving = false;
+                StopAllCoroutines();
+            }
             if (plane != null) {
                 StartCoroutine(planeForward(plane));
+     
             }    
+        }
+
+        //Backward
+        else if (BLPanel.active && BRPanel.active)
+        {
+            if (plane != null)
+            {
+                if (moving)
+                {
+                    moving = false;
+                    StopAllCoroutines();
+                }
+                StartCoroutine(planeBackward(plane));
+            }
+        }
+
+        //Rotate Left
+        else if (ULPanel.active && BRPanel.active)
+        {
+            if (plane != null)
+            {
+                if (rotating)
+                {
+                    rotating = false;
+                    StopCoroutine(planeLeft(plane));
+                    StopCoroutine(planeRight(plane));
+                }
+                StartCoroutine(planeLeft(plane));
+            }
+        }
+
+        //Rotate Right
+        else if (BLPanel.active && URPanel.active)
+        {
+            if (plane != null)
+            {
+                if (rotating)
+                {
+                    rotating = false;
+                    StopCoroutine(planeLeft(plane));
+                    StopCoroutine(planeRight(plane));
+                }
+                StartCoroutine(planeRight(plane));
+            }
         }
     }
 
     IEnumerator planeForward(GameObject plane) {
+        moving = true;
         Debug.Log("Start Forward");
         Vector3 startPos = plane.transform.position;
-        Vector3 endPos = plane.transform.position + plane.transform.forward * movementMult;
+        Vector3 endPos = plane.transform.position + plane.transform.forward * movementMult * 2;
 
         for (float j = 0; j < dur; j += Time.deltaTime)
         {
@@ -56,7 +114,62 @@ public class BatonHandler : MonoBehaviour {
             Debug.Log("Loop");
             yield return null;
         }
-
+        moving = false;
     }
-     
+
+    IEnumerator planeBackward(GameObject plane)
+    {
+        moving = true;
+        Debug.Log("Start Backward");
+        Vector3 startPos = plane.transform.position;
+        Vector3 endPos = plane.transform.position - (plane.transform.forward * movementMult);
+
+        for (float j = 0; j < dur; j += Time.deltaTime)
+        {
+            Vector3 newPos = Vector3.Lerp(startPos, endPos, j / dur);
+            plane.transform.position = newPos;
+            Debug.Log("Loop");
+            yield return null;
+        }
+        moving = false;
+    }
+
+    IEnumerator planeRight(GameObject plane)
+    {
+        rotating = true;
+        Debug.Log("Start Right");
+        Quaternion startRot = plane.transform.rotation;
+        Vector3 endRotEuler = startRot.eulerAngles;
+        endRotEuler.y += 10.0f;
+        Quaternion endRot = Quaternion.Euler(endRotEuler);
+
+
+        for (float i = 0; i < dur; i += Time.deltaTime)
+        {
+            Quaternion newRot = Quaternion.Lerp(startRot, endRot, i / dur);
+            plane.transform.rotation = newRot;
+            yield return null;
+        }
+        rotating = false;
+    }
+
+    IEnumerator planeLeft(GameObject plane)
+    {
+        rotating = true;
+        Debug.Log("Start Left");
+        Quaternion startRot = plane.transform.rotation;
+        Vector3 endRotEuler = startRot.eulerAngles;
+        endRotEuler.y -= 10.0f;
+        Quaternion endRot = Quaternion.Euler(endRotEuler);
+
+
+        for (float i = 0; i < dur; i += Time.deltaTime)
+        {
+            Quaternion newRot = Quaternion.Lerp(startRot, endRot, i / dur);
+            plane.transform.rotation = newRot;
+            yield return null;
+        }
+        rotating = false;
+    }
+
 }
