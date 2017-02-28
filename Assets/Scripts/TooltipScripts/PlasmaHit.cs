db011cf3ac9108d14dac3f38084d94c5ac9fdb98
Wanze Xie <waxie@UCSD.EDU>
2017-02-27 21:52:10 -0800

    beautifully resize down

--- Assets/Scripts/TooltipScripts/PlasmaHit.cs
+++ Assets/Scripts/TooltipScripts/PlasmaHit.cs
@@ -4,9 +4,14 @@ using UnityEngine;
 
 public class PlasmaHit : MonoBehaviour {
 
+
+    public GameObject myRibbon;
+
+    public bool isExecuting;
+
 	// Use this for initialization
 	void Start () {
-		
+        isExecuting = false;
 	}
 	
 	// Update is called once per frame
@@ -16,6 +21,35 @@ public class PlasmaHit : MonoBehaviour {
 
     void OnCollisionEnter(Collision collision)
     {
+
         Debug.Log("I am Plasma, I hitted the plane");
+
+        if(collision.gameObject.tag == "resizable")
+        {
+            GameObject hittedObj = collision.gameObject;
+
+            if (!hittedObj.GetComponent<Resizable>().isLittle)
+            {
+                if (!isExecuting)
+                {
+                    isExecuting = true;
+                    foreach (Transform child in transform) { if (child.gameObject.activeSelf) child.gameObject.SetActive(false); }
+                    StartCoroutine(WaitAndResize(hittedObj));
+                }
+            }
+        }
     }
+
+    IEnumerator WaitAndResize(GameObject hittedObj)
+    {
+        GameObject tempRibbon = (GameObject)Instantiate(myRibbon, hittedObj.transform.position, hittedObj.transform.localRotation);
+        yield return new WaitForSeconds(1f);
+        hittedObj.GetComponent<Resizable>().ResizeDown();
+        yield return new WaitForSeconds(0.5f);
+        Destroy(tempRibbon);
+
+        yield return null;
+    }
+
+    
 }
---
+++ Assets/Scripts/TooltipScripts/Resizable.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Resizable : MonoBehaviour {
+
+
+    public bool isLittle;
+	// Use this for initialization
+	void Start () {
+
+        isLittle = false;
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    
+    public void ResizeDown()
+    {
+        float currScaleValue = transform.localScale.x;
+        float newScaleValue = currScaleValue * 0.25f;
+        transform.localScale = new Vector3(newScaleValue, newScaleValue, newScaleValue);
+        isLittle = true;
+    }
+}
