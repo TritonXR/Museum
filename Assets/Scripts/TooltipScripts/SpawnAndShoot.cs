766d40df1d3a6f6289ff448918cfe7ae3192fd5a
Wanze Xie <waxie@UCSD.EDU>
2017-02-27 20:38:07 -0800

    perfect haptic

--- Assets/Scripts/TooltipScripts/SpawnAndShoot.cs
+++ Assets/Scripts/TooltipScripts/SpawnAndShoot.cs
@@ -4,126 +4,120 @@ using UnityEngine;
 
 public class SpawnAndShoot : MonoBehaviour {
 
-    public GameObject plasma;
-
-    public Transform spawnPos;
-
+    // handle controller input
     public GameObject leftController;
+    private SteamVR_TrackedController leftDevice;
+    private SteamVR_Controller.Device leftDeviceGeneral;
+    private SteamVR_TrackedObject trackedObj;
+    
 
+
+    // handle plasma interaction
+    public GameObject plasma;
+    public Transform spawnPos;
     public float fireForce = 3f;
-
-    private SteamVR_Controller.Device leftDevice;
+    public GameObject arrow;
     private GameObject myPlasma;
     private GameObject myLine;
-
-    private Vector3 zeroVec = new Vector3(0, 0, 0);
     private GameObject hittedObj;
-    private int leftIndex;
+
 
     // Use this for initialization
     void Start () {
-        if (leftController.activeSelf)
-        {
-            leftIndex = SteamVR_Controller.GetDeviceIndex(SteamVR_Controller.DeviceRelation.Leftmost);
-            leftDevice = SteamVR_Controller.Input(leftIndex);
-        }
 
+        leftDevice = leftController.gameObject.GetComponent<SteamVR_TrackedController>();
+        trackedObj = leftController.gameObject.GetComponent<SteamVR_TrackedObject>();
+        //leftDeviceGeneral = leftController.gameObject.GetComponent<SteamVR_TrackedObject>();
         myPlasma = null;
-
-        myLine = new GameObject();
-
-        myLine.AddComponent<LineRenderer>();
-        myLine.SetActive(false);
+        arrow.SetActive(false);
+        // handle trigger events
+        leftDevice.TriggerClicked += TriggerClicked;
+        leftDevice.TriggerUnclicked += TriggerUnClicked;
 
     }
-	
-	// Update is called once per frame
-	void Update () {
-        if(leftDevice == null)
-        {
-            leftDevice = SteamVR_Controller.Input(leftIndex);
-        }
-        if (leftController.activeSelf)
-        {
-            if (leftDevice.GetPressDown(SteamVR_Controller.ButtonMask.Trigger))
-            {
-                if (myPlasma == null)
-                {
-                    myPlasma = (GameObject)Instantiate(plasma, spawnPos.transform.position, spawnPos.transform.localRotation);
-                    myPlasma.GetComponent<Rigidbody>().useGravity = false;
-                    myPlasma.transform.parent = spawnPos.parent;
-                }
 
-                
-            }
-
-            if (leftDevice.GetPress(SteamVR_Controller.ButtonMask.Trigger))
-            {
-                RaycastActivate();
-            }
-            else
-            {
-                myLine.SetActive(false);
-            }
-        }
-
-
-		
-	}
-
-
-
-
-    private void RaycastActivate()
+    void TriggerClicked(object sender, ClickedEventArgs e)
     {
-        myLine.SetActive(true);
-        Ray myRay = new Ray(spawnPos.position, spawnPos.forward);
-        RaycastHit hit;
-
-        if (Physics.Raycast(myRay, out hit, Mathf.Infinity))
-        {
-
-
-            DrawLine(spawnPos.position, hit.point, Color.green);
-
-            if ((hit.collider.gameObject.CompareTag("resizable")))
-            {
-                // show up text, release trigger!
-
-                if (leftDevice.GetPressUp(SteamVR_Controller.ButtonMask.Trigger))
-                {
-                    ShootPlasma();
-                }
-
-
-            }
-
-        }
-        else
-        {
-            DrawLine(zeroVec, zeroVec, Color.green);
-        }
+        CreatePlasma();
+        arrow.SetActive(true);
     }
-
-
-    void DrawLine(Vector3 start, Vector3 end, Color color)
+    
+    void TriggerUnClicked(object sender, ClickedEventArgs e)
     {
-        myLine.transform.position = start;
-        LineRenderer lr = myLine.GetComponent<LineRenderer>();
-        lr.material = new Material(Shader.Find("Particles/Alpha Blended Premultiply"));
-        lr.SetColors(color, color);
-        lr.SetWidth(0.01f, 0.01f);
-        lr.SetPosition(0, start);
-        lr.SetPosition(1, end);
-        //       GameObject.Destroy(myLine, duration);
+        ShootPlasma();
+        arrow.SetActive(false);
     }
+
+    void CreatePlasma()
+    {
+        Debug.Log("I am about to create a plasma");
+        if (myPlasma == null)
+        {
+            myPlasma = (GameObject)Instantiate(plasma, spawnPos.transform.position, spawnPos.transform.localRotation);
+            myPlasma.GetComponent<Rigidbody>().useGravity = false;
+            myPlasma.transform.parent = spawnPos.parent;
+        }
+    }
+
+    private void Update()
+    {
+        leftDeviceGeneral = SteamVR_Controller.Input((int)trackedObj.index);
+    }
+
+
+
+    //private void RaycastActivate()
+    //{
+    //    myLine.SetActive(true);
+    //    Ray myRay = new Ray(spawnPos.position, spawnPos.forward);
+    //    RaycastHit hit;
+
+    //    if (Physics.Raycast(myRay, out hit, Mathf.Infinity))
+    //    {
+
+
+    //        DrawLine(spawnPos.position, hit.point, Color.green);
+
+    //        if ((hit.collider.gameObject.CompareTag("resizable")))
+    //        {
+    //            // show up text, release trigger!
+
+    //            if (leftDevice.GetPressUp(SteamVR_Controller.ButtonMask.Trigger))
+    //            {
+    //                ShootPlasma();
+    //            }
+
+
+    //        }
+
+    //    }
+    //    else
+    //    {
+    //        DrawLine(zeroVec, zeroVec, Color.green);
+    //    }
+    //}
+
+
+    //void DrawLine(Vector3 start, Vector3 end, Color color)
+    //{
+    //    myLine.transform.position = start;
+    //    LineRenderer lr = myLine.GetComponent<LineRenderer>();
+    //    lr.material = new Material(Shader.Find("Particles/Alpha Blended Premultiply"));
+    //    lr.SetColors(color, color);
+    //    lr.SetWidth(0.01f, 0.01f);
+    //    lr.SetPosition(0, start);
+    //    lr.SetPosition(1, end);
+    //    //       GameObject.Destroy(myLine, duration);
+    //}
 
 
     void ShootPlasma()
     {
-        myPlasma.GetComponent<Rigidbody>().velocity = transform.forward * fireForce;
+        Debug.Log("I am going to shoot the plasma ");
+        myPlasma.transform.parent = null;
+        myPlasma.GetComponent<Rigidbody>().velocity = spawnPos.transform.forward * fireForce;
+        leftDeviceGeneral.TriggerHapticPulse(3000);
         Destroy(myPlasma, 10);
-
         myPlasma = null;
     }
 
