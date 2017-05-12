cb48280fece89a5fba18c1f96d692625d80626bf
LJCDVRClub <ljcdvrclub@gmail.com>
2017-05-12 13:42:23 -0700

    Added some scripts and moved things around
    Moved batons closer to center. Added some scripts as potential fix for
    singular pickup bug with planes [not currently workin]. Also added a
    trigger around table for various uses.

---
+++ Assets/Transitions/Scripts/ControllerColliderActive.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerColliderActive : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+    private void OnTriggerEnter(Collider col)
+    {
+        if(col.name == "[CameraRig]")
+        {
+            col.GetComponentInChildren<CapsuleCollider>().enabled = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider col)
+    {
+        col.GetComponentInChildren<CapsuleCollider>().enabled = false;
+    }
+}
