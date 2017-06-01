e958a01514c196f082b45707395b6948546ae251
Dale M Tubat <dtubat@UCSD.EDU>
2017-06-01 13:48:28 -0700

    cleaned up some of the button issues

--- Assets/Scripts/ButtonClicked2.cs
+++ Assets/Scripts/ButtonClicked2.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ButtonClicked2 : MonoBehaviour {
 
@@ -8,23 +9,31 @@ public class ButtonClicked2 : MonoBehaviour {
     public GameObject myRibbon;
     public Transform spawnLoc;
 
+
+    public Text text;
     public bool isExecuting;
 
+
+    private Color tempColor;
 	// Use this for initialization
 	void Start () {
         isExecuting = false;
-	}
+        tempColor = text.color;
+    }
 	
     void OnTriggerEnter(Collider collision)
     {
+
+        text.color = Color.green;
         Debug.Log("Someone collided!@!!!!!!");
         if (collision.gameObject.tag == "Controller")
         {
             Debug.Log("And it is controller");
-
+            text.text = "Big Plane Mode";
             // if plane is big right now
             if (!planeToResize.GetComponent<Resizable>().isLittle)
             {
+
                 if (!planeToResize.GetComponent<Resizable>().isImmune && !planeToResize.GetComponent<Resizable>().isGrabbed)
                 {
                     if (!isExecuting)
@@ -42,6 +51,8 @@ public class ButtonClicked2 : MonoBehaviour {
             {
                 if (!planeToResize.GetComponent<Resizable>().isImmune && !planeToResize.GetComponent<Resizable>().isGrabbed)
                 {
+
+                    text.text = "Toy Plane Mode";
                     if (!isExecuting)
                     {
                         isExecuting = true;
@@ -60,6 +71,11 @@ public class ButtonClicked2 : MonoBehaviour {
         }
     }
 
+
+    private void OnTriggerExit(Collider other)
+    {
+        text.color = tempColor;
+    }
     IEnumerator WaitAndResizeDown(GameObject hittedObj)
     {
         GameObject tempRibbon = (GameObject)Instantiate(myRibbon, hittedObj.transform.position, hittedObj.transform.localRotation);
