b04d6aada38e658d554cf0ff8f6139374e876c5e
Escohen <escohen98@gmail.com>
2017-06-10 19:53:45 -0700

    Plane Physics still needs fixing. Child colliders not functioning and script won't shut off.

--- Assets/Scripts/PlanePhysics.cs
+++ Assets/Scripts/PlanePhysics.cs
@@ -8,31 +8,32 @@ public class PlanePhysics : MonoBehaviour {
 	// Use this for initialization
 	void Awake () {
         rb = gameObject.GetComponent<Rigidbody>();
-       // physicsCol.SetActive(true);
+        physicsCol.SetActive(true);
     }
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-
         //Speeds up slow moving throws
-        if ((System.Math.Pow(rb.velocity.x, 2) + System.Math.Pow(rb.velocity.z, 2) < 1) && rb.velocity != Vector3.zero)
+        if ((System.Math.Pow(rb.velocity.x, 2) + System.Math.Pow(rb.velocity.z, 2) < 1))
         {
             rb.AddForce(rb.transform.forward * 6);
         }
-        rb.AddForce(rb.transform.up * 6);
+        //rb.AddForce(rb.transform.up * 6);
 
         if (rb.velocity != Vector3.zero)
         {
           
             rb.transform.rotation = Quaternion.LookRotation(rb.velocity);
            
-        } else
+        }/* else
         {
+            Debug.Log("Grounded.");
+            grounded = true;
            // gameObject.GetComponent<Rigidbody>().drag = 5;
             gameObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
             gameObject.GetComponent<Rigidbody>().transform.rotation = Quaternion.Euler(0, transform.rotation.eulerAngles.y, 0);
             gameObject.GetComponent<PlanePhysics>().enabled = false;
-        }
+        }*/
 
     }
 }
