715de4eeae01a58b1f16f81276a3fc138f3accea
Escohen <escohen98@gmail.com>
2017-06-20 23:15:42 -0700

    Trying to add warning sign when attempting to go to Part 2 too early.

---
+++ Assets/Scripts/LeaveSceneWarning.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeaveSceneWarning : MonoBehaviour {
+
+    public Canvas warningSign;
+    private float time;
+
+    void OnEnable () {
+        time = 0f;
+	}
+
+    private void Update()
+    {
+        time += Time.deltaTime;
+        Debug.Log("Warning Script Working!");
+    }
+    
+    private void OnTriggerEnter (Collider col) {
+        Debug.Log("Player has entered the warning zone in " + time + " seconds!");
+        Debug.Log(col.name);
+        if (col.name == "WarningCollider" && time < 30f)
+        {
+                warningSign.enabled = true;
+            Debug.Log("Player has entered the warning zone in less than 30 seconds!");
+        }
+	}
+
+    private void OnTriggerExit(Collider col)
+    {
+        if (col.name == "WarningCollider" && warningSign.isActiveAndEnabled)
+        {
+                warningSign.enabled = false;
+                time = 0f; //Resets time to make sure they look at Museum
+            Debug.Log("Player has left the warning zone!");
+        }
+    }
+}
